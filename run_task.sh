a9d030f4564e2db77e8dc2ce484d7ca28588d6d7
Nikola Stojiljkovic <nikolavla@gmail.com>
2018-02-24 14:56:09 +0000

    Configure run_template_all script

--- run_task.sh
+++ run_task.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+#NOTE: Working directory must be root of the project.
+
 if [ "$#" -eq 0 ]; then
     echo "Illegal number of parameters"
+    echo ""
+    echo "Example usage: ./run_task.sh template template/template01.in template/template01.out"
+    echo "Example usage: ./run_task.sh template < ./template/template01.in"
+    echo "Example usage: ./run_task.sh template < ./template/template01.in > ./template/template01.out"
+    echo ""
+    echo "NOTE: Working directory must be root of the project";
     exit 1
 fi
 
@@ -10,6 +18,4 @@ TASK_NAME=$1
 # Shift arguments by 1.
 shift
 
-cd bin
-# echo "Running task: $(pwd)/$TASK_NAME";
-$(echo "./$TASK_NAME $@")
+$(echo "./bin/$TASK_NAME $@")
---
+++ run_template_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TEST_DIR="template"
+FILENAME="template"
+START_TEST_SET=1
+END_TEST_SET=1
+
+if [ "$#" -eq 0 ]; then
+    echo "Running template tests: [$START_TEST_SET, $END_TEST_SET]";
+elif [ "$#" -eq 2 ]; then
+    START_TEST_SET=$1
+    END_TEST_SET=$2
+fi
+
+for test_set_num in $(seq ${START_TEST_SET} ${END_TEST_SET}); do
+    # Figure out filename:
+    if [ ${test_set_num} -lt 10 ]
+    then
+        filename="${TEST_DIR}/${FILENAME}0${test_set_num}"
+    else
+        filename="${TEST_DIR}/${FILENAME}${test_set_num}"
+    fi
+
+    input_filename=$(echo "$filename.in")
+    output_filename=$(echo "$filename.out")
+
+    echo "./run_task.sh template < ${input_filename} > ${output_filename}"
+    ./run_task.sh template < ${input_filename} > ${output_filename}
+done
